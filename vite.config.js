120238da6be304e00a4197898de929d90255a57a
Jeremy Johnson <jeremyjohnson@Jeremys-MacBook-Air.local>
2024-07-24 01:45:31 -0500

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,29 @@
 // vite.config.js
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.NODE_ENV === 'production' ? 'https://black-licorice-800232d1d761.herokuapp.com' : 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    build: {
+      outDir: '../black-licorice-db/public'
     }
-  },
-  build: {
-    outDir: '../black-licorice-db/public'
-  }
+  };
 });
