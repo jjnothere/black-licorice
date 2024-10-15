1a7e125017a29ceb4c4d70c4c5e471a542c843c6
Jeremy Johnson <jjnothere@gmail.com>
2024-10-15 02:02:10 -0500

    removed settings

--- settings.js
+++
@@ -1,8 +0,0 @@
-// settings.js
-
-const settings = {
-    token: 'AQV_sv7464y5sYabV-HsMa9Pn3LPLlP9FwU7Ipu4uQH4Mvc6CgTfcLh2PC26WbI_nscTNnOmSiokgemWAlXG5i-ryx3OLkDMt3IkPG0mlXI6MJDHDlac8bvVjez8iaE3e2VA6xF3eg3aND4b9XrzlPwMU9xXOXHrgxY78dztAUS51ty1LDDc8_zbbmYWtTodY1FruLbvWJrzX2O5cOspK28pMpNAVj348MIitHCNy3bfS4XhjumFcpY8apapvTSyFF__5GVJswxdLzLxcT-CE2cRlenSPKjw4HcMvYgcvO4Glx0Dt_RtPfUmdTEty7vq2KbnQNCiNIQ2ZSbLAdcP8xo0u_lCAg',
-    accountId: '512388408',
-  };
-  
-  module.exports = settings;
